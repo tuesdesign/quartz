f9b651390f90081dfe63ad011882f721b9eac5c0
Constantine Pallas <64446905+tuesdesign@users.noreply.github.com>
2025-02-16 02:50:27 -0500

    Quartz sync: Feb 16, 2025, 2:50 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "constantine.games",
+    baseUrl: "portfolio.constantine.games",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
